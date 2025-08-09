85ee68fb7c19f3188d6c9ce0e4e4237ffbda184d
Richard Li <rhli.richardli@gmail.com>
2025-08-09 18:35:43 -0400

    comparmentalization: new camera page
    revamping old code: moving image stuff to a new page

---
+++ src/app/camera/page.tsx
@@ -0,0 +1,22 @@
+
+import { getSystemDetails } from "@/lib/system";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
+
+export default async function Home() {
+  const systemInfo = await getSystemDetails();
+
+  return (
+    <main className="min-h-screen bg-background flex flex-col items-center justify-center p-6">
+      <h1 className="text-3xl font-bold mb-6 text-foreground">Raspberry Pi</h1>
+
+      <img
+        src="/py/image" //image
+        alt="Raspberry Pi"
+        className="w-4/5"
+       />
+
+
+    </main>
+  );
+}
--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -9,12 +9,6 @@ export default async function Home() {
     <main className="min-h-screen bg-background flex flex-col items-center justify-center p-6">
       <h1 className="text-3xl font-bold mb-6 text-foreground">Raspberry Pi</h1>
 
-      <img
-        src="/py/mjpeg" //image
-        alt="Raspberry Pi"
-        className="w-2/3"
-      />
-
       <Card className="w-full max-w-md">
         <CardHeader>
           <CardTitle>System Information</CardTitle>
