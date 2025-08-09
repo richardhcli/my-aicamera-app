a1f26562df0f440122e6892a4c2dc0d9cf32006f
Richard Li <rhli.richardli@gmail.com>
2025-08-09 17:52:42 -0400

    working code?

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <h1> Richard Li Hello World! </h1>
+      <p style={{backgroundColor: "powderblue", color:"orange", fontFamily:"courier"}}> This is my first raspberry pi hosted website! </p>
+    </main>
+  );
+}
